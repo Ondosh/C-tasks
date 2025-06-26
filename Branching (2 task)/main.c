66508500b62d9eb5697c5547c9d05e7c77d8d582
Ondosh <tima.tomskihh@gmail.com>
2025-06-26 19:09:13 +0900

    did 2nd task

--- Branching (2 task)/main.c
+++ Branching (2 task)/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main() {
-    printf("Задача 2\n");
+    float x, y;
+
+    printf("Пожалуйста введите любой x: ");
+    scanf("%f", &x);
+
+    if (x > 2) {
+        y = 4;
+    }
+    else if (x > -1) {
+        y = pow(x, 2);
+    }
+    else {
+        y = -1/(pow(x, 2));
+    }
+
+    printf("y равен: %f", y);
     return 0;
 }
