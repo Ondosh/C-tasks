c44943813960f11c1882cda1bdeec077640eccb7
Ondosh <sdsd>
2025-06-27 11:45:21 +0900

    3rd task

--- Integer Arithmetic (3 task)/main.c
+++ Integer Arithmetic (3 task)/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Задача 3\n");
+    float a, result, interval;
+    interval = 2;
+
+    puts("Введите а:");
+    scanf("%f", &a);
+    // real number a
+
+    while ((a > 1) || (a < -1)){
+        if (a > 1) {
+            a = a - interval;
+        } else {
+            a = a + interval;
+        }
+    }
+
+    result = pow(a, 2) + 1;
+
+    printf("Относительно начальных условий, а находится на: %f\n", a);
+    printf("Y = %f", result);
     return 0;
 }
